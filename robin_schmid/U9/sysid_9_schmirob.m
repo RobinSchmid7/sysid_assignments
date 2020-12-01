b8520aedbfc81da52a89dc258ad68095bbd9bdd9
Robin Schmid <schmirob@student.ethz.ch>
2020-12-01 10:13:55 +0100

    Fixed exercise 9

--- robin_schmid/U9/sysid_9_schmirob.m
+++ robin_schmid/U9/sysid_9_schmirob.m
@@ -9,12 +9,9 @@ t = 0:N-1;
 
 % Use a different seed to generate input and error noise s.t. they are
 % different
-rng(1);
 u = randn(1,N);
-rng(2);
-e_LS = 0.5*randn(1,N);
 
-threshold = 100;
+threshold = 1000;
 
 Ts = 0.1;
 z = tf('z', Ts);
@@ -24,34 +21,23 @@ B_true = z^-1 - 2*z^-2;
 
 y_true = lsim(B_true/A_true, u); % No noise for true system?
 
-% LS method (biased)
-y_LS = lsim(B_true/A_true, u) + e_LS'; % Attention lsim returns column vector
-
-% IV method (less biased)
-rng(3);
-e_IV = 0.5*randn(1,N);
-y_IV = lsim(B_true/A_true, u) + e_IV';
-
 models = [1, 2; 2, 2; 3, 2; 2, 1; 4, 3];
 n_models = size(models,1);
 
 MSE_LS = zeros(1,n_models);
 MSE_IV = zeros(1,n_models);
 
-y_val_LS = zeros(N,n_models);
-y_val_IV = zeros(N,n_models);
-
-% Validation with new u and y data
-rng(4);
-u_val = randn(1,N);
-rng(5);
-e_val = 0.5*randn(1,N);
+y_est_LS = zeros(N,n_models);
+y_est_IV = zeros(N,n_models);
 
 for k = 1:n_models
     n = models(k,1);
     m = models(k,2);
     
-    % 2. LS method
+    % 2. LS method (biased)
+    e_LS = sqrt(0.5)*randn(1,N);
+    y_LS = lsim(B_true/A_true, u) + e_LS'; % Attention lsim returns column vector
+    
     % Phiy
     Phiy = zeros(N,n);    
     for iy = 1:N
@@ -96,6 +82,9 @@ for k = 1:n_models
     
     % 3. Generate new data, use same u
     % Generate instrumental variable
+    e_IV = sqrt(0.5)*randn(1,N);
+    y_IV = lsim(B_true/A_true, u) + e_IV';
+    
     x = lsim(B_LS/A_LS, u);
     
     % Regressor for instrumental variable
@@ -142,24 +131,26 @@ for k = 1:n_models
     end
     
     % Compare LS and IV method
-    y_val_LS(:,k) = lsim(B_LS/A_LS, u_val) + e_val';
-    y_val_IV(:,k) = lsim(B_IV/A_IV, u_val) + e_val';
+    y_est_LS(:,k) = lsim(B_LS/A_LS, u);
+    y_est_IV(:,k) = lsim(B_IV/A_IV, u);
     
-    MSE_LS(k) = 1/N*norm(y_true - y_val_LS(:,k))^2;
-    MSE_IV(k) = 1/N*norm(y_true - y_val_IV(:,k))^2;
+    MSE_LS(k) = norm(y_true - y_est_LS(:,k))^2;
+    MSE_IV(k) = norm(y_true - y_est_IV(:,k))^2;
     
     % Capture MSE which is too big
-    if MSE_IV(k) > threshold
-        MSE_IV(k) = threshold;
+    if MSE_LS(k) > threshold
+        MSE_LS(k) = inf;
     end
-    
+    if MSE_IV(k) > threshold
+        MSE_IV(k) = inf;
+    end 
 end
 
 % Plot MSE
 figure(1);
-plot(1:n_models, MSE_LS, 'r*-');
+plot(1:n_models, MSE_LS, 'ro--', 'MarkerSize', 10);
 hold on
-plot(1:n_models, MSE_IV, 'b*-');
+plot(1:n_models, MSE_IV, 'bo--', 'MarkerSize', 10);
 title('MSE of LS and IV for different models');
 legend({'LS', 'IV'});
 xlabel('Model');
@@ -172,40 +163,40 @@ figure(2);
 subplot(2,3,1);
 plot(t,y_true,'r');
 hold on
-plot(t,y_val_LS(:,1),'b');
-plot(t,y_val_IV(:,1),'g');
+plot(t,y_est_LS(:,1),'b');
+plot(t,y_est_IV(:,1),'g');
 title('Model 1');
 legend({'True', 'LS', 'IV'});
 % Model 2
 subplot(2,3,2);
 plot(t,y_true,'r');
 hold on
-plot(t,y_val_LS(:,2),'b');
-plot(t,y_val_IV(:,2),'g');
+plot(t,y_est_LS(:,2),'b');
+plot(t,y_est_IV(:,2),'g');
 title('Model 2');
 legend({'True', 'LS', 'IV'});
 % Model 3
 subplot(2,3,3);
 plot(t,y_true,'r');
 hold on
-plot(t,y_val_LS(:,3),'b');
-plot(t,y_val_IV(:,3),'g');
+plot(t,y_est_LS(:,3),'b');
+plot(t,y_est_IV(:,3),'g');
 title('Model 3');
 legend({'True', 'LS', 'IV'});
 % Model 4
 subplot(2,3,4);
 plot(t,y_true,'r');
 hold on
-plot(t,y_val_LS(:,4),'b');
-plot(t,y_val_IV(:,4),'g');
+plot(t,y_est_LS(:,4),'b');
+plot(t,y_est_IV(:,4),'g');
 title('Model 4');
 legend({'True', 'LS', 'IV'});
 % Model 5
 subplot(2,3,5);
 plot(t,y_true,'r');
 hold on
-plot(t,y_val_LS(:,5),'b');
-plot(t,y_val_IV(:,5),'g');
+plot(t,y_est_LS(:,5),'b');
+plot(t,y_est_IV(:,5),'g');
 title('Model 5');
 legend({'True', 'LS', 'IV'});
 
